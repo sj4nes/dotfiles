195d64061554dc4ab051dd0741c1190e5eaab7e0
Simon Janes <23151321+sj4nes@users.noreply.github.com>
2024-04-13 01:45:48 -0500

    nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,147 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+require("opts")
+vim.g.mapleader = " "
+options = { noremap = true }
+vim.api.nvim_set_keymap("n", "<leader>b", ":Telescope buffers<cr>", options)
+vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<cr>", options)
+vim.api.nvim_set_keymap("n", "<leader>n", ":nohlsearch<cr>", options)
+vim.api.nvim_set_keymap("n", "<leader>g", ":Neogit<cr>", options)
+vim.api.nvim_set_keymap("n", "<leader>!", ":BaconLoad<CR>:w<CR>:BaconNext<CR>", options)
+vim.api.nvim_set_keymap("n", "<leader>,", ":BaconList<CR>", options)
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+local plugins = {
+	{ 'Canop/nvim-bacon' },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
+
+	{ "williamboman/mason.nvim" },
+	{ "mhartington/formatter.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		requires = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		ft = { "rust" },
+	},
+}
+require("lazy").setup(plugins, opts)
+require("oil").setup()
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "<leader>a", function()
+	vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+	-- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr })
+
+-- Utilities for creating configurations
+local util = require("formatter.util")
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup({
+	-- Enable or disable logging
+	logging = true,
+	-- Set the log level
+	log_level = vim.log.levels.WARN,
+	-- All formatter configurations are opt-in
+	filetype = {
+		-- Formatter configurations for filetype "lua" go here
+		-- and will be executed in order
+		lua = {
+			-- "formatter.filetypes.lua" defines default configurations for the
+			-- "lua" filetype
+			require("formatter.filetypes.lua").stylua,
+
+			-- You can also define your own configuration
+			function()
+				-- Supports conditional formatting
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
+
+				-- Full specification of configurations is down below and in Vim help
+				-- files
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+
+		-- Use the special "*" filetype for defining formatter configurations on
+		-- any filetype
+		["*"] = {
+			-- "formatter.filetypes.any" defines default configurations for any
+			-- filetype
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+})
+
+vim.cmd("colorscheme catppuccin-mocha")
---
+++ .config/nvim/lua/keys.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<Leader>w', '<cmd>w<cr>')
+vim.keymap.set('n', '<Leader>f', '<cmd>Oil .<cr>')
