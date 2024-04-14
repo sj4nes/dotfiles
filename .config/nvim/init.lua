18726f5c8945f96b8a249d284a65657180d7791c
Simon Janes <23151321+sj4nes@users.noreply.github.com>
2024-04-14 00:04:09 -0500

    tmux nvim fiddling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 local plugins = {
+	{ 'christoomey/vim-tmux-navigator' },
 	{ 'github/copilot.vim' },
 	{ 'Canop/nvim-bacon' },
 	{
