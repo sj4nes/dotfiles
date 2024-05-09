024a027a1741f848ce21a68da2e201af009993e0
Simon Janes <23151321+sj4nes@users.noreply.github.com>
2024-05-09 11:13:51 -0500

    +vlime

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,6 +24,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 local plugins = {
+	{ "vlime/vlime" },
 	{ "mattn/emmet-vim" },
 	{
 		"epwalsh/obsidian.nvim",
