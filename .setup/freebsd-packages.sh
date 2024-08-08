edc3ba6d7cd7e1536624ce2049a3bd6e2316592f
Simon Janes <sjanes@fbsd1.attlocal.net>
2024-08-07 19:08:32 -0500

    +jujutsu

--- .setup/freebsd-packages.sh
+++ .setup/freebsd-packages.sh
@@ -3,7 +3,7 @@ if [ $(id -nu) != "root" ]; then
 	exit
 fi
 
-PKGS=( couchdb3 go fzf inkscape zathura zathura-pdf zathura-cb zathura-djvu sbcl zig syncthing drm-kmod cmake git neovim tmux rust victor-mono-ttf lagrange chromium firefox zoxide kakoune sway swayidle swaylock-effects alacritty kitty dmenu-wayland dmenu wmenu )
+PKGS=( jujutsu couchdb3 go fzf inkscape zathura zathura-pdf zathura-cb zathura-djvu sbcl zig syncthing drm-kmod cmake git neovim tmux rust victor-mono-ttf lagrange chromium firefox zoxide kakoune sway swayidle swaylock-effects alacritty kitty dmenu-wayland dmenu wmenu )
 
 for pkg in "${PKGS[@]}"; do
 	echo "*** $pkg";
